9d1f60b72522df1dd97df21d952abfcd922d537e
CTNOriginals <info@ctn-originals.com>
2026-04-10 13:05:10 +0200

    misc: main startup log

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	var startTime = time.Now()
+	fmt.Printf("\n\n---- go-neural-network START %s ----\n", startTime.Format(time.TimeOnly))
+	defer func() {
+		fmt.Printf("\n---- go-neural-network END %s (%f) ----\n", startTime.Format(time.TimeOnly), time.Since(startTime).Seconds())
+	}()
 }
