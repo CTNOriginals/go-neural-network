8e1d2dbc28085185b19fde2bda2d2b7406442c04
CTNOriginals <info@ctn-originals.com>
2026-04-11 15:44:55 +0200

    refac: activators into map

--- formulas/activators.go
+++ formulas/activators.go
@@ -5,69 +5,72 @@ import (
 	"math"
 )
 
-type TMethod func(x float64) float64
+type TActivator int
+
+const (
+	ReLU TActivator = iota
+	LeakyReLU
+	Sigmoid
+)
+
+var ActivatorNames = []string{
+	"ReLU",
+	"LeakyReLU",
+	"Sigmoid",
+}
+
+type TActivatorFn func(x float64) float64
 
 type Activator struct {
-	Forward  TMethod
-	Backward TMethod
+	Forward  TActivatorFn
+	Backward TActivatorFn
 }
 
-var ReLU = Activator{
-	Forward: func(x float64) float64 {
-		return max(0, x)
-	},
-	Backward: func(x float64) float64 {
-		if x < 0 {
-			return 0
-		}
+var Activators = map[TActivator]Activator{
+	ReLU: {
+		Forward: func(x float64) float64 {
+			return max(0, x)
+		},
+		Backward: func(x float64) float64 {
+			if x < 0 {
+				return 0
+			}
 
-		return 1
-	},
-}
-
-var leakySlope = 0.01
-var LeakyReLU = Activator{
-	Forward: func(x float64) float64 {
-		if x >= 0 {
-			return x
-		}
-
-		return x * leakySlope
-	},
-	Backward: func(x float64) float64 {
-		if x >= 0 {
 			return 1
-		}
-
-		return leakySlope
+		},
 	},
-}
+	LeakyReLU: {
+		Forward: func(x float64) float64 {
+			if x >= 0 {
+				return x
+			}
 
-var Sigmoid = Activator{
-	Forward: func(x float64) float64 {
-		return 1 / (1 + math.Pow(math.E, -x))
+			return x * 0.01
+		},
+		Backward: func(x float64) float64 {
+			if x >= 0 {
+				return 1
+			}
+
+			return 0.01
+		},
 	},
-	Backward: func(x float64) float64 {
-		var sigmoid = 1 / (1 + math.Pow(math.E, -x))
-		return sigmoid * (1 - sigmoid)
+	Sigmoid: {
+		Forward: func(x float64) float64 {
+			return 1 / (1 + math.Pow(math.E, -x))
+		},
+		Backward: func(x float64) float64 {
+			var sigmoid = 1 / (1 + math.Pow(math.E, -x))
+			return sigmoid * (1 - sigmoid)
+		},
 	},
 }
 
 func PrintResults(x float64) {
-	var methods = []Activator{
-		ReLU,
-		LeakyReLU,
-		Sigmoid,
-	}
-	var names = []string{
-		"ReLU",
-		"LeakyReLU",
-		"Sigmoid",
-	}
-
 	fmt.Printf("--- %.2f ----\n", x)
 
-	for i, fn := range methods {
-		fmt.Printf("%s:   \t %.2f\t%.2f\n", names[i], fn.Forward(x), fn.Backward(x))
+	for i, name := range ActivatorNames {
+		var fn = Activators[TActivator(i)]
+		fmt.Printf("%s:   \t %.2f\t%.2f\n", name, fn.Forward(x), fn.Backward(x))
 	}
 }
