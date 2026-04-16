8cb6e17857393596f18d3664e087f6ab7b363eb7
CTNOriginals <info@ctn-originals.com>
2026-04-16 17:47:23 +0200

    func: trainer constructor

--- trainer/trainer.go
+++ trainer/trainer.go
@@ -4,4 +4,14 @@ import "github.com/CTNOriginals/go-neural-network/network"
 
 type Trainer struct {
 	Network *network.Network
+	Data    *TrainingData
+}
+
+func NewTrainer(net *network.Network) *Trainer {
+	var data = make(TrainingData, 0)
+
+	return &Trainer{
+		Network: net,
+		Data:    &data,
+	}
 }
